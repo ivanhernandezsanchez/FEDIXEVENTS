011991f751aa97c7b72849c81aadf880895b13da
Ivan <a27343@svalero.com>
2026-05-19 12:00:06 +0200

    Actualización proyecto

--- ihernandez-frontend/src/components/NotFound.tsx
+++ ihernandez-frontend/src/components/NotFound.tsx
@@ -1,6 +1,6 @@
 function NotFound() {
     return (
-        <div style={{ padding: "40px", backgroundColor: "#fff", height: "100vh" }}>
+        <div style={{ padding: "40px", backgroundColor: "#fff", height: "100vh", color: "#172033" }}>
             <h1>Error 404: Page not found</h1>
             <p>The requested page doesn't exist.</p>
             <a href="/">Back to home</a>
